a91c0b888afbca73b63fbd3150c928106c1be4c2
Felipe <felipeemooreira@gmail.com>
2025-11-18 23:06:32 -0300

    Adição de contexto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
 function Home() {
-  return <h1>Home Page!!</h1>;
+  return (
+    <div>
+      <h1>Para Maria Luiza, Mãe, Avó e em breve Bisavó!</h1>
+      <p style={{
+        ffontFamily: "'Aston Script', cursive",
+        fontStyle: 'italic',
+        fontSize: '18px',
+        lineHeight: '1.5',
+        whiteSpace: 'pre-line'
+      }}>
+        Mãe, lembro da luz em teus olhos<br />
+        que me guiava nos dias incertos.<br />
+        Nos teus braços, encontrei consolo<br />
+        mesmo quando o mundo parecia escuro.<br /><br /> 
+        Conheci tua luta silenciosa,<br />
+        os sacrifícios que no peito guardavas.<br /><br />
+        Hoje, longe, carrego teu amor no peito,<br />
+        e em cada palavra sinto tua presença.<br /><br />
+        Mãe querida, eu te amo. ❤️
+      </p>
+    </div>
+  );
 }
 
 export default Home;
