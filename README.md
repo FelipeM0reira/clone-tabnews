134796a1917e0b426fa9a1a2aeeb6febbb10bda3
Felipe <felipeemooreira@gmail.com>
2025-11-25 19:49:32 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Clone do tabnews para aprendizado e aprimoramento. 
+
+Clone do tabnews para aprendizado e aprimoramento.
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,5 @@
 function Home() {
-  return (
-    <div>
-      <h1>Para Maria Luiza, Mãe, Avó e em breve Bisavó!</h1>
-      <p style={{
-        ffontFamily: "'Aston Script', cursive",
-        fontStyle: 'italic',
-        fontSize: '18px',
-        lineHeight: '1.5',
-        whiteSpace: 'pre-line'
-      }}>
-        Mãe, lembro da luz em teus olhos<br />
-        que me guiava nos dias incertos.<br />
-        Nos teus braços, encontrei consolo<br />
-        mesmo quando o mundo parecia escuro.<br /><br /> 
-        Conheci tua luta silenciosa,<br />
-        os sacrifícios que no peito guardavas.<br /><br />
-        Hoje, longe, carrego teu amor no peito,<br />
-        e em cada palavra sinto tua presença.<br /><br />
-        Mãe querida, eu te amo. ❤️
-      </p>
-    </div>
-  );
+  return <h1>Para pra mim !!!!</h1>;
 }
 
 export default Home;
